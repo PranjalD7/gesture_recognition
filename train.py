4e1c7f00c6ac9c94bc0450a0a8f01efaca4e3a56
Pranjal <pranjal.dwivedi010@gmail.com>
2022-01-06 13:31:12 +0530

    created base model, Loss-Nan issue exists

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from keras.preprocessing.image import ImageDataGenerator
 tf.__version__
-
+from PIL import Image
 
 train_datagen = ImageDataGenerator(
         rescale=1./255,
@@ -15,7 +15,30 @@ training_set = train_datagen.flow_from_directory(r'C:/Users/pranj/OneDrive/Deskt
                                                  class_mode = 'sparse')
 test_datagen = ImageDataGenerator(rescale = 1./255)
 test_set = test_datagen.flow_from_directory(r'C:/Users/pranj/OneDrive/Desktop/ges_dataset/archive (2)/test/test',
-                                            target_size = (64, 64),
+                                            target_size = (50, 50),
                                             batch_size = 32,
                                             class_mode = 'binary')
 
+cnn = tf.keras.models.Sequential()
+
+cnn.add(tf.keras.layers.Conv2D(filters=64, kernel_size=3, activation='relu', input_shape=[50, 50, 3]))
+cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
+
+cnn.add(tf.keras.layers.Conv2D(filters=64, kernel_size=3, activation='relu'))
+cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
+
+cnn.add(tf.keras.layers.Flatten())
+
+# Step 4 - Full Connection
+cnn.add(tf.keras.layers.Dense(units=128, activation='relu'))
+
+# Step 5 - Output Layer
+cnn.add(tf.keras.layers.Dense(units=19, activation='softmax'))
+
+# Part 3 - Training the CNN
+
+# Compiling the CNN
+cnn.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
+
+# Training the CNN on the Training set and evaluating it on the Test set
+cnn.fit(x = training_set, validation_data = test_set, epochs = 25)
